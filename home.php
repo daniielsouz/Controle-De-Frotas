688d696cc90f8d9a401fadfa53bcddd9dd0d8e69
H DevJr <hdevjr@gmail.com>
2024-08-09 12:45:48 -0300

    ciração página principal

---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <header>
+            <div class="logo">
+                <a href="home.php">Logo</a>
+            </div>
+            <ul class="nav-itens">
+                <li><a href="home.php">Home</a></li>
+                <li><a href="hitórico.php">Histórico</a></li>
+                <li><a href="suporte">Suporte</a></li>
+</body>
+</html>
